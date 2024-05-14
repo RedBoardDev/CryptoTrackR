9afdee33155e436a9f6e216b949b6cae5499717c
RedBoardDev <ott.thomas68@gmail.com>
2024-05-14 21:55:08 +0200

    feat(server setup): add morgan module

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
-
+import morgan from 'morgan';
+// https://github.com/hapijs/joi
+  
 dotenv.config();
 
 const app = express();
@@ -11,5 +13,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
+app.use(morgan('dev'));
 
 export default app;
